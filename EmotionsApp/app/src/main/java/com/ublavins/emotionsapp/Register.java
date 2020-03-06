2ae44ab58fd303c459732b182e5fc150ccdd3460
u-blavins <usama.blavins1@gmail.com>
2020-03-06 12:51:52 +0000

    Add new project; Update old project

--- EmotionsApp/app/src/main/java/com/ublavins/emotionsapp/Register.java
+++ EmotionsApp/app/src/main/java/com/ublavins/emotionsapp/Register.java
@@ -25,31 +25,31 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mDisplayDate = (TextView) findViewById(R.id.dobText);
-
-        mDisplayDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Calendar calendar = Calendar.getInstance();
-                int year = calendar.get(Calendar.YEAR);
-                int month = calendar.get(Calendar.MONTH);
-                int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog dialog = new DatePickerDialog(
-                        Register.this,
-                        android.R.style.Theme_Material_Dialog_MinWidth,
-                        mDateSetListener,
-                        year, month, day);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
-            }
-        });
-
-        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                Log.d(TAG, month + "/" + day + "/" + year);
-            }
-        };
+//        mDisplayDate = (TextView) findViewById(R.id.dobText);
+//
+//        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Calendar calendar = Calendar.getInstance();
+//                int year = calendar.get(Calendar.YEAR);
+//                int month = calendar.get(Calendar.MONTH);
+//                int day = calendar.get(Calendar.DAY_OF_MONTH);
+//
+//                DatePickerDialog dialog = new DatePickerDialog(
+//                        Register.this,
+//                        android.R.style.Theme_Material_Dialog_MinWidth,
+//                        mDateSetListener,
+//                        year, month, day);
+//                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//                dialog.show();
+//            }
+//        });
+//
+//        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+//                Log.d(TAG, month + "/" + day + "/" + year);
+//            }
+//        };
     }
 }
