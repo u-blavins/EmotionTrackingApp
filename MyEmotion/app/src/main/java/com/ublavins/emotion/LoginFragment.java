75e381b92d4e490f4867089e9ca41b4649e1bcf6
u-blavins <usama.blavins1@gmail.com>
2020-03-09 23:14:37 +0000

    Update app with registration fields, validation and connectivity to Firebase Auth and Firestore

--- MyEmotion/app/src/main/java/com/ublavins/emotion/LoginFragment.java
+++ MyEmotion/app/src/main/java/com/ublavins/emotion/LoginFragment.java
@@ -10,7 +10,6 @@ import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
--- MyEmotion/app/src/main/java/com/ublavins/emotion/RegisterFragment.java
+++ MyEmotion/app/src/main/java/com/ublavins/emotion/RegisterFragment.java
@@ -3,23 +3,38 @@ package com.ublavins.emotion;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.tiper.MaterialSpinner;
 
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -30,10 +45,13 @@ import java.util.Calendar;
 public class RegisterFragment extends Fragment {
 
     private AuthCallback callback;
-    private TextInputLayout fnameInput, lnameInput, emailInputLayout, passwordInput,
-        dateLayout;
+    private TextInputLayout fnameInput, lnameInput, emailInput, passwordInput,
+        dateInput;
     private TextInputEditText fname, lname, email, password, date;
     private MaterialButton signUp;
+    private static final String[] GENDERS = {"Male", "Female", "Other"};
+    private MaterialSpinner gender;
+    private FirebaseAuth mAuth;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -48,6 +66,7 @@ public class RegisterFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         callback = (AuthCallback)getActivity();
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -55,11 +74,22 @@ public class RegisterFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_register, container, false);
-        fname = (TextInputEditText) view.findViewById(R.id.fnameRegister);
-        lname = (TextInputEditText) view.findViewById(R.id.lnameRegister);
+        fnameInput = (TextInputLayout)view.findViewById(R.id.fnameInputLayout);
+        lnameInput = (TextInputLayout)view.findViewById(R.id.lnameInputLayout);
+        emailInput = (TextInputLayout)view.findViewById(R.id.emailInputLayout);
+        passwordInput = (TextInputLayout)view.findViewById(R.id.passwordInputLayout);
+        dateInput = (TextInputLayout)view.findViewById(R.id.dobInputLayout);
+        fname = (TextInputEditText)view.findViewById(R.id.fnameRegister);
+        lname = (TextInputEditText)view.findViewById(R.id.lnameRegister);
         email = (TextInputEditText)view.findViewById(R.id.emailRegister);
         password = (TextInputEditText)view.findViewById(R.id.passwordRegister);
         date = (TextInputEditText)view.findViewById(R.id.dobRegister);
+        gender = (MaterialSpinner)view.findViewById(R.id.genderRegister);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_dropdown_item_1line, GENDERS);
+        gender.setAdapter(adapter);
+        signUp = (MaterialButton)view.findViewById(R.id.signUpButton);
+
 
         date.setOnClickListener(
                 new View.OnClickListener() {
@@ -69,14 +99,47 @@ public class RegisterFragment extends Fragment {
                     }
                 }
         );
-//        signUp = (MaterialButton)view.findViewById(R.id.signUpButton);
+
+        signUp.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        register();
+                    }
+                }
+        );
 
         return view;
     }
 
     public void register() {
         if (validateRegister()) {
-
+            String emailText = email.getText().toString();
+            String passText = password.getText().toString();
+            final Map<String, Object> user = new HashMap<>();
+            user.put("FirstName", fname.getText().toString());
+            user.put("LastName", lname.getText().toString());
+            user.put("Email", emailText);
+            user.put("Dob", date.getText().toString());
+            user.put("Gender", gender.getSelectedItem().toString());
+            mAuth.createUserWithEmailAndPassword(user.get("Email").toString(), passText)
+                    .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseFirestore.getInstance().collection("Users")
+                                        .document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        makeToast("Account created");
+                                    }
+                                });
+                            } else {
+                                makeToast("Account already exists");
+                            }
+                        }
+                    });
         }
     }
 
@@ -102,6 +165,51 @@ public class RegisterFragment extends Fragment {
         String lnameText = lname.getText().toString();
         String emailText = email.getText().toString();
         String passText = password.getText().toString();
+        String dateText = date.getText().toString();
+        String genderText;
+
+        if (fnameText.isEmpty()) {
+            fnameInput.setError("Field must not be empty");
+            isValid = false;
+        } else {
+            fnameInput.setError(null);
+        }
+
+        if (lnameText.isEmpty()) {
+            lnameInput.setError("Field must not be empty");
+            isValid = false;
+        } else {
+            lnameInput.setError(null);
+        }
+
+        if (emailText.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
+            emailInput.setError("Enter a valid email address");
+            isValid = false;
+        } else {
+            emailInput.setError(null);
+        }
+
+        if (passText.isEmpty()) {
+            passwordInput.setError("Enter a password");
+            isValid = false;
+        } else {
+            passwordInput.setError(null);
+        }
+
+        if (dateText.isEmpty()) {
+            dateInput.setError("Enter a password");
+            isValid = false;
+        } else {
+            dateInput.setError(null);
+        }
+
+        try {
+            genderText = gender.getSelectedItem().toString();
+            gender.setError(null);
+        } catch (NullPointerException ex) {
+            gender.setError("Select a gender");
+        }
+
         return isValid;
     }
 
