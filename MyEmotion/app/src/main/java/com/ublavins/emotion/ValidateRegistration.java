af5ffd24b72e73fcaabf516f91000592bf4b4cc0
u-blavins <usama.blavins1@gmail.com>
2020-04-02 00:32:20 +0100

    Update app with unit testing for registration validation

---
+++ MyEmotion/app/src/main/java/com/ublavins/emotion/ValidateRegistration.java
@@ -0,0 +1,152 @@
+package com.ublavins.emotion;
+
+import android.util.Patterns;
+
+
+public class ValidateRegistration {
+
+    public class ValidationMessage {
+
+        private boolean check;
+        private String message;
+
+        public ValidationMessage(boolean bool, String msg) {
+            check = bool;
+            message = msg;
+        }
+
+        public void setCheck(boolean bool) {
+            check = bool;
+        }
+
+        public void setMessage(String msg) {
+            message = msg;
+        }
+
+        public boolean getCheck() {
+            return check;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+    }
+
+    private String firstNameVal;
+    private String lastNameVal;
+    private String emailVal;
+    private String passwordVal;
+    private String dobVal;
+    private String genderVal;
+
+    public ValidateRegistration(
+            String fName,
+            String lName,
+            String email,
+            String pass,
+            String dob,
+            String gender
+    ) {
+        firstNameVal = fName;
+        lastNameVal = lName;
+        emailVal = email;
+        passwordVal = pass;
+        dobVal = dob;
+        genderVal = gender;
+    }
+
+    public void setFirstNameVal(String fName) {
+        firstNameVal = fName;
+    }
+
+    public void setLastNameVal(String lName) {
+        lastNameVal = lName;
+    }
+
+    public void setEmailVal(String email) {
+        emailVal = email;
+    }
+
+    public void setPasswordVal(String pass) {
+        passwordVal = pass;
+    }
+
+    public void setDobVal(String dob) {
+        dobVal = dob;
+    }
+
+    public void setGenderVal(String gender) {
+        genderVal = gender;
+    }
+
+    public String getFirstNameVal() {
+        return firstNameVal;
+    }
+
+    public String getLastNameVal() {
+        return lastNameVal;
+    }
+
+    public String getEmailVal() {
+        return emailVal;
+    }
+
+    public String getPasswordVal() {
+        return passwordVal;
+    }
+
+    public String getDobVal() {
+        return dobVal;
+    }
+
+    public String getGenderVal() {
+        return genderVal;
+    }
+
+    public ValidationMessage validateFirstName() {
+        ValidationMessage validationMessage = new ValidationMessage(true, "");
+        if (firstNameVal.isEmpty()) {
+            validationMessage.setCheck(false);
+            validationMessage.setMessage("Field must not be empty");
+        } else if (!firstNameVal.matches("^[a-zA-Z]+$")) {
+            validationMessage.setCheck(false);
+            validationMessage.setMessage("First name must contain letters");
+        }
+        return validationMessage;
+    }
+
+    public ValidationMessage validateLastName() {
+        ValidationMessage validationMessage = new ValidationMessage(true, "");
+        if (lastNameVal.isEmpty()) {
+            validationMessage.setCheck(false);
+            validationMessage.setMessage("Field must not be empty");
+        } else if (!lastNameVal.matches("^[a-zA-Z]+$")) {
+            validationMessage.setCheck(false);
+            validationMessage.setMessage("Last name must contain letters");
+        }
+        return validationMessage;
+    }
+
+    public ValidationMessage validateEmail() {
+        ValidationMessage validationMessage = new ValidationMessage(true, "");
+        if (emailVal.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(emailVal).matches()) {
+            validationMessage.setCheck(false);
+            validationMessage.setMessage("Field must not be empty");
+        }
+        return validationMessage;
+    }
+
+    public ValidationMessage validatePassword() {
+        ValidationMessage validationMessage = new ValidationMessage(true, "");
+        if (passwordVal.isEmpty()) {
+            validationMessage.setCheck(false);
+            validationMessage.setMessage("Field must not be empty");
+        } else if (passwordVal.length() < 8) {
+            validationMessage.setCheck(false);
+            validationMessage.setMessage("Password must have a minimum length of 8 characters");
+        }
+        return validationMessage;
+    }
+
+}
---
+++ MyEmotion/app/src/test/java/com/ublavins/emotion/ValidateRegistrationTest.java
@@ -0,0 +1,175 @@
+package com.ublavins.emotion;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidateRegistrationTest {
+
+    private ValidateRegistration validateRegistration;
+    private String mockFirstName;
+    private String mockLastName;
+    private String mockEmail;
+    private String mockPassword;
+    private String mockDob;
+    private String mockGender;
+
+    @Before
+    public void setUp() {
+        mockFirstName = "";
+        mockLastName = "";
+        mockEmail = "";
+        mockPassword = "";
+        mockDob = "";
+        mockGender = "";
+        validateRegistration = new ValidateRegistration(
+                mockFirstName, mockLastName, mockEmail,
+                mockPassword, mockDob, mockGender);
+    }
+
+    @After
+    public void tearDown() {
+        validateRegistration = null;
+    }
+
+    @Test
+    public void setGetFirstName() {
+        String expected = "test";
+        validateRegistration.setFirstNameVal(expected);
+        String sut = validateRegistration.getFirstNameVal();
+        assertEquals(expected, sut);
+    }
+
+    @Test
+    public void setGetLastName() {
+        String expected = "test";
+        validateRegistration.setLastNameVal(expected);
+        String sut = validateRegistration.getLastNameVal();
+        assertEquals(expected, sut);
+    }
+
+    @Test
+    public void setGetEmail() {
+        String expected = "test";
+        validateRegistration.setEmailVal(expected);
+        String sut = validateRegistration.getEmailVal();
+        assertEquals(expected, sut);
+    }
+
+    @Test
+    public void setGetPassword() {
+        String expected = "test";
+        validateRegistration.setPasswordVal(expected);
+        String sut = validateRegistration.getPasswordVal();
+        assertEquals(expected, sut);
+    }
+
+    @Test
+    public void setGetDob() {
+        String expected = "test";
+        validateRegistration.setDobVal(expected);
+        String sut = validateRegistration.getDobVal();
+        assertEquals(expected, sut);
+    }
+
+    @Test
+    public void setGetGender() {
+        String expected = "test";
+        validateRegistration.setGenderVal(expected);
+        String sut = validateRegistration.getGenderVal();
+        assertEquals(expected, sut);
+    }
+
+    @Test
+    public void validateEmptyFirstName() {
+        String fakeFirstName = "";
+        validateRegistration.setFirstNameVal(fakeFirstName);
+        ValidateRegistration.ValidationMessage sut = validateRegistration.validateFirstName();
+        assertFalse(sut.getCheck());
+        assertEquals("Field must not be empty", sut.getMessage());
+    }
+
+    @Test
+    public void validateInvalidFirstName() {
+        String fakeFirstName = "12345";
+        validateRegistration.setFirstNameVal(fakeFirstName);
+        ValidateRegistration.ValidationMessage sut = validateRegistration.validateFirstName();
+        assertFalse(sut.getCheck());
+        assertEquals("First name must contain letters", sut.getMessage());
+    }
+
+    @Test
+    public void validateValidFirstName() {
+        String fakeFirstName = "Test";
+        validateRegistration.setFirstNameVal(fakeFirstName);
+        ValidateRegistration.ValidationMessage sut = validateRegistration.validateFirstName();
+        assertTrue(sut.getCheck());
+        assertEquals("", sut.getMessage());
+    }
+
+    @Test
+    public void validateEmptyLastName() {
+        String fakeLastName = "";
+        validateRegistration.setLastNameVal(fakeLastName);
+        ValidateRegistration.ValidationMessage sut = validateRegistration.validateLastName();
+        assertFalse(sut.getCheck());
+        assertEquals("Field must not be empty", sut.getMessage());
+    }
+
+    @Test
+    public void validateInvalidLastName() {
+        String fakeLastName = "12345";
+        validateRegistration.setLastNameVal(fakeLastName);
+        ValidateRegistration.ValidationMessage sut = validateRegistration.validateLastName();
+        assertFalse(sut.getCheck());
+        assertEquals("Last name must contain letters", sut.getMessage());
+    }
+
+    @Test
+    public void validateValidLastName() {
+        String fakeLastName = "Test";
+        validateRegistration.setLastNameVal(fakeLastName);
+        ValidateRegistration.ValidationMessage sut = validateRegistration.validateLastName();
+        assertTrue(sut.getCheck());
+        assertEquals("", sut.getMessage());
+    }
+
+    @Test
+    public void validateInvalidEmail() {
+        String fakeEmail = "";
+        validateRegistration.setEmailVal(fakeEmail);
+        ValidateRegistration.ValidationMessage sut = validateRegistration.validateEmail();
+        assertFalse(sut.getCheck());
+        assertEquals("Field must not be empty", sut.getMessage());
+    }
+
+    @Test
+    public void validateEmptyPassword() {
+        String fakePassword = "";
+        validateRegistration.setPasswordVal(fakePassword);
+        ValidateRegistration.ValidationMessage sut = validateRegistration.validatePassword();
+        assertFalse(sut.getCheck());
+        assertEquals("Field must not be empty", sut.getMessage());
+    }
+
+    @Test
+    public void validateInvalidPassword() {
+        String fakePassword = "test";
+        validateRegistration.setPasswordVal(fakePassword);
+        ValidateRegistration.ValidationMessage sut = validateRegistration.validatePassword();
+        assertFalse(sut.getCheck());
+        assertEquals("Password must have a minimum length of 8 characters", sut.getMessage());
+    }
+
+    @Test
+    public void validateValidPassword() {
+        String fakePassword = "Password";
+        validateRegistration.setPasswordVal(fakePassword);
+        ValidateRegistration.ValidationMessage sut = validateRegistration.validatePassword();
+        assertTrue(sut.getCheck());
+        assertEquals("", sut.getMessage());
+    }
+
+}
